58e0b304bfb936fca6127fe895b3637ea3bcfc6d
Jhorkman Bernal <jhorber95@gmail.com>
2019-02-10 23:12:18 -0500

    search post

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
-  get 'search/index'
   get 'home/index'
   devise_for :users
 
   root to: 'home#index'
   resources :users, only: %i[show edit update]
   resources :posts, only: %i[new create show destroy]
+
+  get 'search' => 'search#index'
 end
